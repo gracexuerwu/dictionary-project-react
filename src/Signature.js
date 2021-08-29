bcf5d8d743f352fde835e072dbb07af83954b2dc
gracexuerwu <grace.xuer.wu@gmail.com>
2021-08-29 11:15:08 +0200

    Minor signature styling

--- src/Signature.js
+++ src/Signature.js
@@ -4,7 +4,7 @@ import './Signature.css';
 export default function Signature() {
     return (
         <div className="Signature">
-            © Open-source code <i className="fab fa-github fa-lg"></i> by Grace Wu
+            © <a href="https://github.com/gracexuerwu/dictionary-project" target="_blank" rel="noreferrer" className="signatureGitLink">Open-source code</a> <i className="fab fa-github fa-lg"></i> by Grace Wu
         </div>
     );
 }
