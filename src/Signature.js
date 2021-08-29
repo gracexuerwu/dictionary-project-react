c0af1bb70bac462e6b6510dc0e22dbf7134c55b4
gracexuerwu <grace.xuer.wu@gmail.com>
2021-08-29 11:17:27 +0200

    Minor styling fix

--- src/Signature.js
+++ src/Signature.js
@@ -4,7 +4,7 @@ import './Signature.css';
 export default function Signature() {
     return (
         <div className="Signature">
-            © <a href="https://github.com/gracexuerwu/dictionary-project" target="_blank" rel="noreferrer" className="signatureGitLink">Open-source code</a> <i className="fab fa-github fa-lg"></i> by Grace Wu
+            © 2021 <a href="https://github.com/gracexuerwu/dictionary-project-react" target="_blank" rel="noreferrer" className="signatureGitLink">Open-source code</a> <i className="fab fa-github fa-lg"></i> by Grace Wu
         </div>
     );
 }
