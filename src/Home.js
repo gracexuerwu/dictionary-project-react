8292e9cc0e2a9fe5ca07a9cb642a41dfe9fb44d3
gracexuerwu <grace.xuer.wu@gmail.com>
2021-08-31 21:26:52 +0200

    Home styling

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import './Home.css';
+import Lottie from 'lottie-react';
+import nyanCat from "./animation/nyanCat.json";
 
 export default function Home() {
-    return (
-        <div>
-            <div>
-              <a href="/Search" >Let's start AAA</a>
-            </div>
-            <div>
-              <a href="/About" >About BBB</a>
-            </div>
+  return (
+    <div className="Home">
+      <div className="Home-header">
+        {/* <Lottie animationData={nyanCat} className="nyanCat" /> */}
+        <div className="dictionary">
+          Dictionary
         </div>
-    );
+
+        <div>
+        </div>
+      </div >
+      <div className="controls">
+        <a href="/Search" className="Search-control" >PRESS START</a>
+        <div>
+          <a href="/About" className="About-control">ABOUT</a>
+        </div>
+      </div>
+    </div >
+  );
 }
