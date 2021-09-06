b45f0ab4112bfef43fb9779f39705361d7f1a3ee
gracexuerwu <grace.xuer.wu@gmail.com>
2021-09-06 14:42:33 +0200

    Styled Meaning component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,22 +1,29 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import './Meaning.css';
+
 
 export default function Meaning(props) {
     return (
         <div className="Meaning">
-            <h3>{props.meaning.partOfSpeech}</h3>
-            {props.meaning.definitions.map((definition, index) => {
-                return (
-                    <div key={index}>
-                        <p>
-                            Definition: {definition.definition}
-                            <br />
-                            <em>Examples: {definition.example}</em>
-                            <Synonyms synonyms={definition.synonyms} />
-                        </p>
-                    </div>
-                );
-            })}
+            <div class="row">
+                <div className="col-2 typeOfWord">{props.meaning.partOfSpeech}</div>
+                <div className="col-10 definitionan">
+                    {props.meaning.definitions.map((definition, index) => {
+                        return (
+                            <div key={index}>
+                                <p>
+                                    Definition: {definition.definition}
+                                    <br />
+                                    <p>Examples: {definition.example}</p>
+                                    <Synonyms synonyms={definition.synonyms} />
+                                </p>
+                            </div>
+                        );
+                    })}
+                </div>
+
+            </div>
 
         </div>
     );
