b7de99c66f34d73e621608457280a0d208220a30
gracexuerwu <grace.xuer.wu@gmail.com>
2021-09-13 21:23:34 +0200

    Minor changes

--- src/About.js
+++ src/About.js
@@ -19,13 +19,13 @@ export default function About() {
                     ABOUT
                 </h1>
                 <p>
-                    THIS DICTIONARY PROJECT WAS BUILT BY <a href="https://www.linkedin.com/in/grace-ade-wu/">GRACE WU</a> AS A FUN and LIGHTHEARTED PROJECT TO LEARN AND PRACTICE THE NICKS AND NACKS OF REACT.JS.
+                    THIS DICTIONARY PROJECT WAS BUILT BY <a href="https://www.linkedin.com/in/grace-ade-wu/" target="_blank" rel="noreferrer">GRACE WU</a> AS A FUN and LIGHTHEARTED PROJECT TO LEARN AND PRACTICE THE NICKS AND NACKS OF REACT.JS.
                 </p>
                 <h2>
                     CREDITS
                 </h2>
                 <p>
-                    THIS PROJECT WAS BUILT WITH FREE API(S) IN COURTESY OF <a href="https://dictionaryapi.dev/">FREE DICTIONARY API</a> AND <a href="https://www.pexels.com/api/">PEXELS</a>
+                    THIS PROJECT WAS BUILT WITH FREE API(S) IN COURTESY OF <a href="https://dictionaryapi.dev/" target="_blank" rel="noreferrer">FREE DICTIONARY API</a> AND <a href="https://www.pexels.com/api/" target="_blank" rel="noreferrer">PEXELS</a>
                 </p>
             </div>
         </div >
